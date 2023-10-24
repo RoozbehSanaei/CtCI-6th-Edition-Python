0defa0a7d1d1cfa7e5585239550197ac0dd361d3
Roozbeh Sanaei <Roozbeh.Sanaei2@Gmail.com>
2023-10-24 16:25:54 +0800

    Update p05_sort_stack.py

--- chapter_03/p05_sort_stack.py
+++ chapter_03/p05_sort_stack.py
@@ -3,19 +3,29 @@ import unittest
 
 from chapter_03.stack import Stack
 
-
+'''
+Write a program to sort a stack such that the smallest items are on the top. You can use
+an additional temporary stack, but you may not copy the elements into any other data structure
+(such as an array). The stack supports the following operations: push, pop, peek, and is Empty.
+'''
 class SortedStack(Stack):
     def __init__(self):
         super().__init__()
         self.temp_stack = Stack()
 
     def push(self, item):
+        # If the stack is empty or the item is smaller than the top item, simply push it
         if self.is_empty() or item < self.peek():
             super().push(item)
         else:
+            # Move items larger than 'item' from the main stack to the temporary stack
             while self.peek() is not None and item > self.peek():
                 self.temp_stack.push(self.pop())
+
+            # Push the new item to the main stack
             super().push(item)
+
+            # Move the items back from the temporary stack to the main stack
             while not self.temp_stack.is_empty():
                 super().push(self.temp_stack.pop())
 
