8020411e535b6a08d7156d098fc40800e9389688
Roozbeh Sanaei <Roozbeh.Sanaei2@Gmail.com>
2023-10-24 15:43:06 +0800

    Update p03_stack_of_plates.py

--- chapter_03/p03_stack_of_plates.py
+++ chapter_03/p03_stack_of_plates.py
@@ -13,126 +13,110 @@ Implement a function popAt ( int index) which performs a pop operation on a spec
 
 
 # Define a Node class to represent an individual element in a stack
+'''
+The above and below pointers in the Node class serve to connect each node to its adjacent nodes within a stack, forming a doubly-linked list. This makes it easier to navigate and manipulate the stack.
+above: Points to the node that is one position above the current node in the stack. If the node is at the top of the stack, above would be None.
+below: Points to the node that is one position below the current node in the stack. If the node is at the bottom of the stack, below would be None.
+'''
+
 class Node:
     def __init__(self, value):
-        self.value = value  # The value of the node
-        self.above = None  # A reference to the node above this one in the stack
-        self.below = None  # A reference to the node below this one in the stack
+        self.value = value
+        self.above = None
+        self.below = None
 
-# Define a Stack class with a maximum capacity
+
 class Stack:
     def __init__(self, capacity):
-        self.capacity = capacity  # The maximum number of elements the stack can hold
-        self.size = 0  # The current number of elements in the stack
-        self.top = None  # The top element in the stack
-        self.bottom = None  # The bottom element in the stack
+        self.capacity = capacity
+        self.size = 0
+        self.top = None
+        self.bottom = None
 
-    # Check if the stack is full
     def is_full(self):
         return self.size == self.capacity
 
-    # Check if the stack is empty
     def is_empty(self):
         return self.size == 0
 
-    # Join two nodes by setting their above and below pointers
     def join(self, above, below):
         if below:
             below.above = above
         if above:
             above.below = below
 
-    # Push a new value onto the stack
     def push(self, v):
-        # If stack is full, cannot push new element
         if self.size >= self.capacity:
             return False
         self.size += 1
-        n = Node(v)  # Create a new node
-        # If the stack was empty, this new node becomes the bottom
+        n = Node(v)
         if self.size == 1:
             self.bottom = n
-        # Connect the new node to the previous top node
         self.join(n, self.top)
-        # Update the top pointer to the new node
         self.top = n
         return True
 
-    # Pop the top value off the stack
     def pop(self):
         if not self.top:
             return None
-        t = self.top  # Get the current top node
-        self.top = self.top.below  # Move the top pointer down
-        self.size -= 1  # Decrement the size
-        return t.value  # Return the value of the popped node
+        t = self.top
+        self.top = self.top.below
+        self.size -= 1
+        return t.value
 
-    # Remove the bottom value from the stack
     def remove_bottom(self):
-        b = self.bottom  # Get the current bottom node
-        self.bottom = self.bottom.above  # Move the bottom pointer up
+        b = self.bottom
+        self.bottom = self.bottom.above
         if self.bottom:
             self.bottom.below = None
-        self.size -= 1  # Decrement the size
-        return b.value  # Return the value of the removed node
+        self.size -= 1
+        return b.value
 
-# Define a SetOfStacks class that comprises multiple stacks
+
 class SetOfStacks:
     def __init__(self, capacity):
-        self.capacity = capacity  # The maximum capacity for each individual stack
-        self.stacks = []  # The list holding all individual stacks
+        self.capacity = capacity
+        self.stacks = []
 
-    # Get the last stack in the set
     def get_last_stack(self):
         if not self.stacks:
             return None
-        return self.stacks[-1]  # Return the last stack
+        return self.stacks[-1]
 
-    # Check if the set of stacks is empty
     def is_empty(self):
         last = self.get_last_stack()
         return not last or last.is_empty()
 
-    # Push a new value onto the last stack or a new stack
     def push(self, v):
         last = self.get_last_stack()
-        # If there is a last stack and it's not full, push the value
         if last and not last.is_full():
             last.push(v)
         else:
-            # Create a new stack and push the value
             stack = Stack(self.capacity)
             stack.push(v)
             self.stacks.append(stack)
 
-    # Pop the top value off the last stack
     def pop(self):
         last = self.get_last_stack()
         if not last:
             return None
         v = last.pop()
-        # If the last stack becomes empty, remove it from the list of stacks
         if last.size == 0:
             del self.stacks[-1]
-        return v  # Return the popped value
+        return v
 
-    # Pop an element from a stack at a specific index
     def pop_at(self, index):
         return self.left_shift(index, True)
 
-    # Shift an element out of a stack to balance it after a pop_at
     def left_shift(self, index, remove_top):
-        stack = self.stacks[index]  # Get the stack at the given index
-        # Pop the top or bottom element based on remove_top flag
+        stack = self.stacks[index]
         removed_item = stack.pop() if remove_top else stack.remove_bottom()
-        # If the stack is empty after removal, delete it
         if stack.is_empty():
             del self.stacks[index]
-        # If there are more stacks to the right, shift an element from the next stack
         elif len(self.stacks) > index + 1:
             v = self.left_shift(index + 1, False)
-            stack.push(v)  # Push the shifted value to the current stack
-        return removed_item  # Return the removed item
+            stack.push(v)
+        return removed_item
 
 class Tests(unittest.TestCase):
     def test_stacks(self):
