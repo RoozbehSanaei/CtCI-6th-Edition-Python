f0b4ef0b77284cf5d695ae901d07f8f4eba0afa0
Roozbeh Sanaei <Roozbeh.Sanaei2@Gmail.com>
2023-10-27 07:02:30 +0800

    Update p10_check_subtree.py

--- chapter_04/p10_check_subtree.py
+++ chapter_04/p10_check_subtree.py
@@ -1,5 +1,11 @@
 from chapter_04.binary_tree import BinaryTree, Node
 
+'''
+Check Subtree: Tl and T2 are two very large binary trees, with Tl much bigger than T2. Create an
+algorithm to determine if T2 is a subtree of Tl.
+A tree T2 is a subtree of Tl if there exists a node n in Tl such that the subtree of n is identical to T2.
+That is, if you cut off the tree at node n, the two trees would be identical.
+'''
 
 class ComparableTreeNode(Node):
     def __eq__(self, other):
@@ -24,6 +30,11 @@ def is_subtree(haystack_tree, needle_tree):
         return False
     return _is_subtree(haystack_tree.root, needle_tree.root)
 
+'''
+The _is_subtree function recursively searches the larger tree to find if a node matches the root of the smaller tree, along with its descendants. 
+It first checks if either node is None and returns False if so. Then it checks for a match. If no match is found, 
+it continues the search in the left and right children of the current node in the larger tree.
+'''
 
 def _is_subtree(haystack_node, needle_node):
     if haystack_node is None or needle_node is None:
