a0eedafb93b1effbc7d0eea9f15649710cdfbfca
Roozbeh Sanaei <Roozbeh.Sanaei2@Gmail.com>
2023-10-26 19:49:45 +0800

    Update p08_first_common_ancestor.py

--- chapter_04/p08_first_common_ancestor.py
+++ chapter_04/p08_first_common_ancestor.py
@@ -1,5 +1,19 @@
 from chapter_04.binary_tree import BinaryTree
 
+'''
+First Common Ancestor: Design an algorithm and write code to find the first common ancestor
+of two nodes in a binary tree. Avoid storing additional nodes in a data structure. NOTE: This is not
+necessarily a binary search tree.
+'''
+
+
+
+'''
+Check for Null: If either p or q is null, return None.
+Get Depths: Calculate the depths of p and q from the root.
+Equalize Depths: Move the deeper node up the tree to the same level as the shallower node.
+Find Ancestor: Traverse upwards from both nodes until they meet at the common ancestor.
+'''
 
 def first_common_ancestor(p, q):
     if not p or not q:
