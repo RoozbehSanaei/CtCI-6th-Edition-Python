e484bbc82d52e01e90776dc24fa1231d72d81e02
Roozbeh Sanaei <Roozbeh.Sanaei2@Gmail.com>
2023-10-26 14:14:18 +0800

    Update p05_validate_bst.py

--- chapter_04/p05_validate_bst.py
+++ chapter_04/p05_validate_bst.py
@@ -1,9 +1,26 @@
 from chapter_04.binary_search_tree import BinarySearchTree
 from chapter_04.binary_tree import BinaryTree
 
+'''
+Validate BST: Implement a function to check if a binary tree is a binary search tree.
+Specifically, for a binary tree to be a BST:
+
+    The left subtree of a node contains only nodes with keys less than the node's key.
+    The right subtree of a node contains only nodes with keys greater than the node's key.
+    Both the left and right subtrees must also be binary search trees.
+'''
 
 def is_binary_search_tree(tree):
     return _is_bst(tree.root)
+
+
+'''
+The algorithm uses a recursive approach to traverse the tree and check these properties. It does so by keeping track of the minimum (min_val) and maximum (max_val) allowable values for each node as it traverses down the tree.
+
+When going left, the max_val is updated to the current node's value.
+When going right, the min_val is updated to the current node's value.
+By doing this, it checks not just local conditions (i.e., comparing a node with its immediate children) but also more global conditions that consider ancestors and descendants, which is crucial for validating a BST.
+'''
 
 
 def _is_bst(node, min_val=None, max_val=None):
