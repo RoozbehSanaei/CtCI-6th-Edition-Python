99e68e330414b36bd79a7b7f66066356eda2cdca
Roozbeh Sanaei <Roozbeh.Sanaei2@Gmail.com>
2023-10-27 08:02:08 +0800

    Update p01_insertion.py

--- chapter_05/p01_insertion.py
+++ chapter_05/p01_insertion.py
@@ -1,3 +1,11 @@
+'''
+Insertion: You are given two 32-bit numbers, N and M, and two bit positions, i and
+j. Write a method to insert M into N such that M starts at bit j and ends at bit i. You
+can assume that the bits j through i have enough space to fit all of M. That is, if
+M = 10011, you can assume that there are at least 5 bits between j and i. You would not, for
+example, have j = 3 and i = 2, because M could not fully fit between bit 3 and bit 2.
+'''
+
 def bits_insertion(n, m, i, j):
     ones_left = -1 << (j + 1)  # shift 1s over to the left, before position j
     ones_right = (1 << i) - 1  # place 1s to the right of position i
