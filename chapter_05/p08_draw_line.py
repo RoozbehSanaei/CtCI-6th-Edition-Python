55447202f03eff72630c796e44f9dc126743db36
Roozbeh Sanaei <Roozbeh.Sanaei2@Gmail.com>
2023-10-27 16:38:43 +0800

    Update p08_draw_line.py

--- chapter_05/p08_draw_line.py
+++ chapter_05/p08_draw_line.py
@@ -1,3 +1,11 @@
+'''
+Draw Line: A monochrome screen is stored as a single array of bytes, allowing eight consecutive
+pixels to be stored in one byte. The screen has width w, where w is divisible by 8 (that is, no byte will
+be split across rows). The height of the screen, of course, can be derived from the length of the array
+and the width. Implement a function that draws a horizontal line from ( xl, y) to ( x2, y).
+'''
+
+
 def draw_line(screen: bytearray, width: int, x1: int, x2: int, y: int) -> None:
     left_byte, right_byte = (y * width + x1) // 8, (y * width + x2) // 8
     left_mask, right_mask = 0xFF >> x1 % 8, (0xFF >> x2 % 8 + 1) ^ 0xFF
