1091a25a006c376e88604e1de351e678c0ac0ba2
Roozbeh Sanaei <Roozbeh.Sanaei2@Gmail.com>
2023-10-28 10:52:44 +0800

    Update p04_power_set.py

--- chapter_08/p04_power_set.py
+++ chapter_08/p04_power_set.py
@@ -1,5 +1,8 @@
 import copy
 
+'''
+Power Set: Write a method to return all subsets of a set.
+'''
 
 # Solution using recursion
 def get_subsets_a(setz, index=None):
@@ -11,12 +14,15 @@ def get_subsets_a(setz, index=None):
     old_subs = get_subsets_a(setz, index - 1)
     new_subs = []
     item = setz[index]
+    '''
+    The loop iterates over all existing subsets, appending both the original subset and a new subset that includes the current item, 
+    '''
     for val in old_subs:
-        new_subs.append(val)
-        # List is mutable
-        entry = copy.deepcopy(val)
-        entry.append(item)
-        new_subs.append(entry)
+        new_subs.append(val) # Append the old_subs
+        entry = copy.deepcopy(val)  # Create a deep copy to avoid altering the original subset
+        entry.append(item)  # Add the new element to this copy
+        new_subs.append(entry)  # Append the new subset
+
 
     return new_subs
 
@@ -30,17 +36,17 @@ def get_subsets_b(aset):
         all_subsets.append(subset)
     return all_subsets
 
-
 def convert_int_to_set(x, aset):
     subset = []
     index = 0
-    k = x
+    k = x  
     while k > 0:
-        if k & 1 == 1 and aset[index] not in subset:
-            subset.append(aset[index])
-        index += 1
-        k >>= 1
+        if k & 1 == 1 and aset[index] not in subset:  # Check if the least-significant bit is 1 and the element is not in 'subset'
+            subset.append(aset[index])  # Append the corresponding element from 'aset' to 'subset'
+        index += 1  
+        k >>= 1  # Right-shift 'k' to check the next bit
     return subset
+
 
 
 # alternative recursive solution.
